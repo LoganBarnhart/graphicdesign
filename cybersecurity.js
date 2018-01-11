011b28ba7b09b4e6043b6367434df3ee7572c1ad
bluefox <bluefox@github.com>
2018-01-11 12:05:09 -0700

    Initial commit

---
+++ cybersecurity.js
@@ -0,0 +1,22 @@
+
+
+function display() {
+
+	document.getElementById("popD").innerHTML = "Art";
+	document.getElementById("popD2").innerHTML = "";
+	
+	
+	
+
+}
+
+function display2(){
+	document.getElementById("popD").innerHTML = " ";
+	document.getElementById("popD2").innerHTML = " ";
+	document.getElementById("popD3").innerHTML = " ";	
+	
+	
+	
+	
+}
+
